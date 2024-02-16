4b7e3955edc620449826c313d52068a170c15a74
rodrigo-seabra <rodrigo.seabra08@outlook.com>
2024-02-16 14:52:47 -0300

    teste deploy impossivel

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
 //configuração da resposta em json
 app.use(express.json()); //nao precisa do url encoded pois só irá se comunicar em json mesmo
 
@@ -13,10 +14,12 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.static("public"));
 app.use("/public", express.static("public"));
 
-const UserRoutes = require("./routes/UserRoutes");
-const ProductRoutes = require("./routes/ProductRoutes");
+const UserRoutes = require("../routes/UserRoutes");
+const ProductRoutes = require("../routes/ProductRoutes");
 
 app.use("/users", UserRoutes); //rotas de user
 app.use("/product", ProductRoutes); //rotas de produtos
 
-app.listen(5000);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
