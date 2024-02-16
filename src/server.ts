0e1cea542b49a57434a3f3469c131799fa91d146
rodrigo-seabra <rodrigo.seabra08@outlook.com>
2024-02-16 16:59:12 -0300

    Teste deploy render

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,23 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 //configuração da resposta em json
 app.use(express.json()); //nao precisa do url encoded pois só irá se comunicar em json mesmo
 
 //Solve cors - resolvendo o problema de cors
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3333" }));
 
 //public folder for images - pasta publica para imagens
 app.use(express.static("public"));
 app.use("/public", express.static("public"));
 
-const UserRoutes = require("../routes/UserRoutes");
-const ProductRoutes = require("../routes/ProductRoutes");
+const UserRoutes = require("./routes/UserRoutes");
+const ProductRoutes = require("./routes/ProductRoutes");
 
 app.use("/users", UserRoutes); //rotas de user
 app.use("/product", ProductRoutes); //rotas de produtos
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen({
+  host: "0.0.0.0",
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
 });
